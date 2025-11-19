141af6b84e92219a2c0c3984c4b020a2f6af4fff
Michael <mvasilyev04@gmail.com>
2025-11-19 18:56:29 +0300

    remove godotenv, now just os.Getenv

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,40 +1,7 @@
 package config
 
-import (
-	"log"
-	"os"
+import "os"
 
-	"github.com/joho/godotenv"
-)
-
-type DConfig struct {
-	Port string
-}
-
-type DBConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DBName   string
-}
-
-func Config() *DConfig {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	return &DConfig{Port: os.Getenv("PORT")}
-}
-
-func DbConfig() *DBConfig {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("Error: db .env file not found")
-	}
-	return &DBConfig{
-		Host:     os.Getenv("DBHOST"),
-		Port:     os.Getenv("DBPORT"),
-		User:     os.Getenv("DBUSER"),
-		Password: os.Getenv("DBPASSWORD"),
-		DBName:   os.Getenv("DBNAME"),
-	}
+func GetString(key string) string {
+	return os.Getenv(key)
 }
