876268cb2af5cd506b25c832de731d580bc9940a
Michael <mvasilyev04@gmail.com>
2025-11-20 19:11:36 +0300

    rewrite queries to db

--- internal/database/user_storage.go
+++ internal/database/user_storage.go
@@ -14,12 +14,12 @@ type UserRepository interface {
 }
 
 func (d *Database) Register(context context.Context, user *models.User) error {
-	var existID uint64
-	err := d.db.QueryRow(context, "SELECT id FROM users WHERE email=$1", user.Email).Scan(&existID)
-	if err != nil && existID > 0 {
-		return fmt.Errorf("user already exists %d", existID)
+	var existID bool
+	_ = d.db.QueryRow(context, "SELECT id FROM users WHERE email=$1", user.Email).Scan(&existID)
+	if existID {
+		return fmt.Errorf("user already exists %t", existID)
 	}
-	_, err = d.db.Exec(context,
+	_, err := d.db.Exec(context,
 		"INSERT INTO users (first_name, last_name, email, password_hash, role) VALUES ($1, $2, $3, $4, $5)",
 		user.FirstName, user.LastName, user.Email, user.PasswordHash, user.Role)
 	if err != nil {
@@ -30,17 +30,14 @@ func (d *Database) Register(context context.Context, user *models.User) error {
 
 func (d *Database) Login(context context.Context, user *models.User) (*models.User, error) {
 	var storedHash string
-	err := d.db.QueryRow(context, "SELECT password_hash FROM users WHERE email=$1 ORDER BY id DESC LIMIT 1", user.Email).Scan(&storedHash)
+	err := d.db.QueryRow(context, `SELECT first_name, last_name, password_hash, role FROM users WHERE email=$1`, user.Email).
+		Scan(&user.FirstName, &user.LastName, &storedHash, &user.Role)
 	if err != nil {
-		return nil, fmt.Errorf("user not found")
+		return nil, errors.New("Failed to select datas from users")
 	}
 	ok, err := services.CheckPasswordHash(user.PasswordHash, storedHash)
 	if !ok {
 		return nil, fmt.Errorf("%v", err)
-	}
-	err = d.db.QueryRow(context, `SELECT first_name, last_name, role FROM users WHERE email=$1`, user.Email).Scan(&user.FirstName, &user.LastName, &user.Role)
-	if err != nil {
-		return nil, errors.New("Failed to select datas from users")
 	}
 	return user, nil
 }
