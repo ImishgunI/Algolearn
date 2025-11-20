9832d93079d01d2d061486de7950e1fe4f3a94e9
Michael <mvasilyev04@gmail.com>
2025-11-20 03:15:25 +0300

    create routes

---
+++ internal/routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"algolearn/internal/controller"
+	"algolearn/internal/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetRoutes(r *gin.Engine, db *database.Database) {
+	h := controller.NewUserHandler(db)
+	hc := controller.NewCommentHandler(db)
+	hl := controller.NewLessonHandler(db)
+	r.POST("/register", h.RegisterController)
+	r.POST("/login", h.LoginController)
+	r.POST("/newComment", hc.AddComment)
+	r.GET("/lessons", hl.GetLessons)
+}
