d30333488acbd5e8f23933d5e10d3e7a1c57c806
Michael <126423992+ImishgunI@users.noreply.github.com>
2025-12-03 02:58:13 +0300

    did methods for setting lessons done

--- internal/database/lesson_storage.go
+++ internal/database/lesson_storage.go
@@ -3,10 +3,12 @@ package database
 import (
 	"algolearn/internal/models"
 	"context"
+	"time"
 )
 
 type LessonsRepository interface {
 	GetLessons(ctx context.Context) ([]*models.Lesson, error)
+	SetLessonComplete(ctx context.Context, email, lessonTitle string) error
 }
 
 func (d *Database) GetLessons(ctx context.Context) ([]models.Lesson, error) {
@@ -23,4 +25,19 @@ func (d *Database) GetLessons(ctx context.Context) ([]models.Lesson, error) {
 		result = append(result, l)
 	}
 	return result, nil
+}
+
+func (d *Database) SetLessonComplete(ctx context.Context, email, lessonTitle string) error {
+	var user_id int
+	err := d.db.QueryRow(ctx, `SELECT id FROM users where email = $1`, email).Scan(&user_id)
+
+	if err != nil {
+		return err
+	}
+	completed_at := time.Now()
+	_, err = d.db.Exec(ctx, `INSERT INTO completed_lessons (user_id, lesson_title, completed_at) VALUES ($1, $2, $3)`, user_id, lessonTitle, completed_at)
+	if err != nil {
+		return err
+	}
+	return nil
 }
