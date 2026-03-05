d01d08b25cb5001d1a6405e610350856ca0e89b5
michael <mvasilyev04@gmail.com>
2026-03-05 14:02:06 +0300

    add graceful shutdown and fiber app creation

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"Algolearn/internal/transport/http"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v3"
+)
+
 func main() {
 
+	app := fiber.New()
+	http.Routes(app)
+	go func() {
+		if err := app.Listen(":8000"); err != nil {
+			log.Fatalf("listen error %+v\n", err)
+		}
+	}()
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigchan
+	log.Printf("Shutdown signal recieved")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		log.Printf("Shutdown error %+v\n", err)
+	}
 }
