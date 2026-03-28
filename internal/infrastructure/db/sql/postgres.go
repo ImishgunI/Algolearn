d039af5652fe23dec83367d0aed2cc59f954ffd1
Michael <mvasilyev04@gmail.com>
2026-03-28 22:58:15 +0300

    create implementation for postgres

---
+++ internal/infrastructure/db/sql/postgres.go
@@ -0,0 +1,21 @@
+package sql
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Postgres struct {
+	pool *pgxpool.Pool
+}
+
+func NewPool(ctx context.Context, connectionString string) (*Postgres, error) {
+	conn, err := pgxpool.New(ctx, connectionString)
+	if err != nil {
+		return nil, err
+	}
+	return &Postgres{
+		pool: conn,
+	}, nil
+}
