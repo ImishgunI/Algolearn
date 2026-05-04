990348471f3a3be15ed7f4da6c5fe1e8c5609e4a
Michael <mvasilyev04@gmail.com>
2026-05-05 00:14:44 +0300

    implement manager

--- internal/execution/manager/manager.go
+++ internal/execution/manager/manager.go
@@ -1,1 +1,30 @@
 package manager
+
+import (
+	"Algolearn/internal/execution/algorithms"
+	"Algolearn/internal/execution/state"
+	"fmt"
+)
+
+type Manager struct {
+	algorithms map[string]algorithms.Algorithm
+}
+
+func New() *Manager {
+	return &Manager{
+		algorithms: map[string]algorithms.Algorithm{
+			"bubble_sort": &algorithms.BubbleSort{},
+		},
+	}
+}
+
+func (m *Manager) Execute(algorithm string, data []int) ([]state.Step, error) {
+	algo, ok := m.algorithms[algorithm]
+	if !ok {
+		return nil, fmt.Errorf("unknown algorithm")
+	}
+
+	steps := algo.Run(data)
+
+	return steps, nil
+}
