9165c1289ee50e5eece5c1ba57d3b1ec4a35467e
Michael <mvasilyev04@gmail.com>
2026-05-04 17:40:42 +0300

    Почистил метод

--- internal/transport/http/registration.go
+++ internal/transport/http/registration.go
@@ -2,24 +2,31 @@ package http
 
 import (
 	"Algolearn/internal/auth"
-	"Algolearn/internal/infrastructure/db"
-	"Algolearn/internal/transport"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 type Registration struct {
-	uc *db.UserCreator
+	service *auth.AuthService
+}
+
+func NewRegistration(service *auth.AuthService) *Registration {
+	return &Registration{
+		service: service,
+	}
 }
 
 func (r *Registration) SignUp(c fiber.Ctx) error {
-	var userInfo transport.UserRegistrationInfo
+	var userInfo auth.RegisterInput
+
 	if err := c.Bind().Body(&userInfo); err != nil {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
-	auth.CreateToken()
-	// hash password
-	// go to db
-	// error handling
+
+	err := r.service.Register(c.Context(), userInfo)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
 	return c.SendStatus(fiber.StatusCreated)
 }
