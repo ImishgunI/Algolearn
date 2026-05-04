4fd0ccda1b0f417ce588105c1b75924ff0bd2e29
Michael <mvasilyev04@gmail.com>
2026-05-05 02:18:07 +0300

    change method Execute and add method get

--- internal/execution/manager/manager.go
+++ internal/execution/manager/manager.go
@@ -3,28 +3,45 @@ package manager
 import (
 	"Algolearn/internal/execution/algorithms"
 	"Algolearn/internal/execution/state"
+	"Algolearn/internal/execution/storage"
+	"context"
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 type Manager struct {
 	algorithms map[string]algorithms.Algorithm
+	storage    *storage.Storage
 }
 
-func New() *Manager {
+func New(storage *storage.Storage) *Manager {
 	return &Manager{
+		storage: storage,
 		algorithms: map[string]algorithms.Algorithm{
 			"bubble_sort": &algorithms.BubbleSort{},
 		},
 	}
 }
 
-func (m *Manager) Execute(algorithm string, data []int) ([]state.Step, error) {
+func (m *Manager) Execute(ctx context.Context, algorithm string, data []int) (string, error) {
 	algo, ok := m.algorithms[algorithm]
 	if !ok {
-		return nil, fmt.Errorf("unknown algorithm")
+		return "", fmt.Errorf("unknown algorithm")
 	}
 
 	steps := algo.Run(data)
 
-	return steps, nil
+	execID := uuid.New().String()
+
+	err := m.storage.SaveSteps(ctx, execID, steps)
+	if err != nil {
+		return "", err
+	}
+
+	return execID, nil
+}
+
+func (m *Manager) Get(ctx context.Context, execID string) ([]state.Step, error) {
+	return m.storage.GetSteps(ctx, execID)
 }
