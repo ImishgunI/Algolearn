c4403481f81b184d01f16212b5a3d78338e70f43
Michael <mvasilyev04@gmail.com>
2026-05-04 18:46:56 +0300

    add new interface SessionRepository

--- internal/infrastructure/db/interfaces.go
+++ internal/infrastructure/db/interfaces.go
@@ -3,9 +3,17 @@ package db
 import (
 	"Algolearn/internal/users"
 	"context"
+	"time"
 )
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *users.User) error
 	GetByEmail(ctx context.Context, email string) (*users.User, error)
+	GetByID(ctx context.Context, id int) (*users.User, error)
+}
+
+type SessionRepository interface {
+	CreateSession(ctx context.Context, userID int, token string, expiresAt time.Time) error
+	GetSession(ctx context.Context, token string) (int, error)
+	DeleteSession(ctx context.Context, token string) error
 }
