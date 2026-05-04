86dbfae431b0a9339b355a831acd78041fcef845
Michael <mvasilyev04@gmail.com>
2026-05-04 18:19:50 +0300

    united registration and authorization in user_repository

---
+++ internal/infrastructure/db/userauth/user_repository.go
@@ -0,0 +1,42 @@
+package userauth
+
+import (
+	"Algolearn/internal/infrastructure/db/sql"
+	"Algolearn/internal/users"
+	"context"
+)
+
+type Repository struct {
+	p *sql.Postgres
+}
+
+func NewRepo(db *sql.Postgres) *Repository {
+	return &Repository{
+		p: db,
+	}
+}
+
+func (r *Repository) CreateUser(ctx context.Context, user_data *users.User) error {
+	const query = `INSERT INTO users (user_name, user_surname, email, password_hash, role)
+		VALUES ($1, $2, $3, $4, $5)`
+
+	_, err := r.p.Pool.Exec(ctx, query,
+		user_data.Name, user_data.Surname, user_data.Email, user_data.PasswordHash, user_data.Role)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) GetByEmail(ctx context.Context, email string) (*users.User, error) {
+	const query = `SELECT id, first_name, last_name, email, password_hash, role
+					FROM users
+					WHERE email = $1`
+	var user_info users.User
+	err := r.p.Pool.QueryRow(ctx, query, email).Scan(&user_info.ID, &user_info.Name, &user_info.Surname, &user_info.Email, &user_info.PasswordHash, &user_info.Role)
+	if err != nil {
+		return nil, err
+	}
+	return &user_info, nil
+}
