d80deb929e1e2cac15f6e4882f8b98bbd87fcc32
Michael <mvasilyev04@gmail.com>
2026-05-04 18:23:23 +0300

    add id in struct

--- internal/users/user.go
+++ internal/users/user.go
@@ -1,9 +1,10 @@
 package users
 
 type User struct {
-	Name         string `json:"name"`
-	Surname      string `json:"surname"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"password_hash"`
-	Role         string `json:"role"`
+	ID           int    `json:"id" db:"id"`
+	Name         string `json:"name" db:"first_name"`
+	Surname      string `json:"surname" db:"last_name"`
+	Email        string `json:"email" db:"email"`
+	PasswordHash string `json:"password_hash" db:"password_hash"`
+	Role         string `json:"role" db:"role"`
 }
