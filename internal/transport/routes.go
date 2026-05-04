11f4bd6f6a136fe161e045a2014b89723a7a438c
Michael <mvasilyev04@gmail.com>
2026-05-04 18:48:05 +0300

    add middleware

--- internal/transport/routes.go
+++ internal/transport/routes.go
@@ -1,6 +1,7 @@
 package transport
 
 import (
+	"Algolearn/internal/middleware"
 	"Algolearn/internal/transport/http"
 
 	"github.com/gofiber/fiber/v3"
@@ -11,6 +12,15 @@ func Routes(reg *http.Registration, auth *http.Authorization) *fiber.App {
 
 	app.Post("/registration", reg.SignUp)
 	app.Post("/login", auth.SignIn)
+	app.Post("/refresh", auth.Refresh)
+
+	app.Get("/me", middleware.JWTMiddleware("secret"), func(c fiber.Ctx) error {
+		userID := c.Locals("user_id")
+
+		return c.JSON(fiber.Map{
+			"user_id": userID,
+		})
+	})
 
 	return app
 }
