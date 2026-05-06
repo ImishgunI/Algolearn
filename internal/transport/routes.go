5a5ea6be0902e76336fad81710f9c24798c5e083
Michael <mvasilyev04@gmail.com>
2026-05-06 18:07:38 +0300

    add routes

--- internal/transport/routes.go
+++ internal/transport/routes.go
@@ -5,22 +5,17 @@ import (
 	"Algolearn/internal/transport/http"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func Routes(reg *http.Registration, auth *http.Authorization, exec *http.ExecutionHandler) *fiber.App {
 	app := fiber.New()
-
+	app.Use(cors.New())
 	app.Post("/registration", reg.SignUp)
 	app.Post("/login", auth.SignIn)
 	app.Post("/refresh", auth.Refresh)
 
-	app.Get("/me", middleware.JWTMiddleware("secret"), func(c fiber.Ctx) error {
-		userID := c.Locals("user_id")
-
-		return c.JSON(fiber.Map{
-			"user_id": userID,
-		})
-	})
+	app.Get("/me", middleware.JWTMiddleware("secret"), auth.Me)
 
 	app.Post("/execute", exec.Execute)
 	app.Get("/execution/:id", exec.Get)
