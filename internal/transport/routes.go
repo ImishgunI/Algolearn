9a97cf1322ee8a253d5407974e9ad1026c52279d
Michael <mvasilyev04@gmail.com>
2026-05-06 21:18:47 +0300

    add group /api

--- internal/transport/routes.go
+++ internal/transport/routes.go
@@ -11,13 +11,14 @@ import (
 func Routes(reg *http.Registration, auth *http.Authorization, exec *http.ExecutionHandler) *fiber.App {
 	app := fiber.New()
 	app.Use(cors.New())
-	app.Post("/registration", reg.SignUp)
-	app.Post("/login", auth.SignIn)
-	app.Post("/refresh", auth.Refresh)
+	api := app.Group("/api")
+	api.Post("/register", reg.SignUp)
+	api.Post("/login", auth.SignIn)
+	api.Post("/refresh", auth.Refresh)
 
-	app.Get("/me", middleware.JWTMiddleware("secret"), auth.Me)
+	api.Get("/me", middleware.JWTMiddleware("secret"), auth.Me)
 
-	app.Post("/execute", exec.Execute)
-	app.Get("/execution/:id", exec.Get)
+	api.Post("/execute", exec.Execute)
+	api.Get("/execution/:id", exec.Get)
 	return app
 }
