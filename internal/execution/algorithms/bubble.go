1e50dc792e098de0135060a8857160ea1a39098f
Michael <mvasilyev04@gmail.com>
2026-05-06 18:06:09 +0300

    add 2 arrays for Step structute

--- internal/execution/algorithms/bubble.go
+++ internal/execution/algorithms/bubble.go
@@ -12,18 +12,21 @@ func (b *BubbleSort) Run(input []int) []state.Step {
 
 	n := len(arr)
 
-	for i := 0; i < n; i++ {
+	for i := range n {
 		for j := 0; j < n-i-1; j++ {
 
 			if arr[j] > arr[j+1] {
-
+				active := []int{j, j + 1}
+				swap := []int{j, j + 1}
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 
 				snapshot := make([]int, len(arr))
 				copy(snapshot, arr)
 
 				steps = append(steps, state.Step{
-					Array: snapshot,
+					Array:    snapshot,
+					Active:   active,
+					Swapping: swap,
 				})
 			}
 		}
