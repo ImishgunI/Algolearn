5cf98f7f739981fcef2f0386911dcd3b12822883
Michael <mvasilyev04@gmail.com>
2026-05-10 22:59:29 +0300

    Добавил таблица избранных и комментов

--- migrations/0004_create_table_lessons.up.sql
+++ migrations/0004_create_table_lessons.up.sql
@@ -7,3 +7,17 @@ CREATE TABLE IF NOT EXISTS lessons (
     order_index INT NOT NULL DEFAULT 0,
     created_at TIMESTAMPTZ DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS comments (
+    id SERIAL PRIMARY KEY,
+    lesson_id INT NOT NULL REFERENCES lessons(id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    body TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS favorites (
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    lesson_id INT NOT NULL REFERENCES lessons(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, lesson_id)
+);
