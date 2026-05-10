f94ea94cd4f7a1dca8c7bc84c3404dec0141e0ea
Michael <mvasilyev04@gmail.com>
2026-05-10 23:27:57 +0300

    прописал маршруты

--- internal/transport/routes.go
+++ internal/transport/routes.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gofiber/fiber/v3/middleware/logger"
 )
 
-func Routes(reg *http.Registration, auth *http.Authorization, exec *http.ExecutionHandler) *fiber.App {
+func Routes(reg *http.Registration, auth *http.Authorization, exec *http.ExecutionHandler, ls *http.LessonHandler, ch *http.CommentHandler, fh *http.FavoriteHandler) *fiber.App {
 	app := fiber.New()
 	app.Use(logger.New())
 	app.Use(cors.New())
@@ -21,5 +21,14 @@ func Routes(reg *http.Registration, auth *http.Authorization, exec *http.Executi
 
 	app.Post("/execute", exec.Execute)
 	app.Get("/execution/:id", exec.Get)
+
+	app.Get("/courses/:courseID/lessons", ls.GetByCourse)
+	app.Get("/lessons/:id", ls.GetByID)
+
+	app.Get("/lessons/:lessonID/comments", ch.GetByLesson)
+	app.Post("/lessons/:lessonID/comments", middleware.JWTMiddleware("secret"), ch.Create)
+
+	app.Get("/lessons/:lessonID/favorite", middleware.JWTMiddleware("secret"), fh.Status)
+	app.Post("/lessons/:lessonID/favorite", middleware.JWTMiddleware("secret"), fh.Toggle)
 	return app
 }
