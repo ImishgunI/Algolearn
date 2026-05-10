7caef4a9b6ccc599a6490a22e913186768d2d8f2
Michael <mvasilyev04@gmail.com>
2026-05-10 22:59:04 +0300

    Добавил обработчик и работу с бд для комментариев

---
+++ internal/infrastructure/db/comment/comment_repository.go
@@ -0,0 +1,49 @@
+package comment
+
+import (
+	"Algolearn/internal/infrastructure/db/sql"
+	"Algolearn/internal/learning/comment"
+	"context"
+	"time"
+)
+
+type Repository struct {
+	p *sql.Postgres
+}
+
+func NewCommentRepository(db *sql.Postgres) *Repository {
+	return &Repository{
+		p: db,
+	}
+}
+
+func (r *Repository) Create(ctx context.Context, c *comment.Comment) error {
+	c.CreatedAt = time.Now().UTC()
+	_, err := r.p.Pool.Exec(ctx,
+		"INSERT INTO comments (lesson_id, user_id, body, created_at) VALUES ($1, $2, $3, $4)",
+		c.LessonID, c.UserID, c.Body, c.CreatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) GetByLesson(ctx context.Context, lessonID int) ([]comment.Comment, error) {
+	rows, err := r.p.Pool.Query(ctx, `SELECT c.id, c.user_id, u.user_name, c.body, c.created_at from comments c
+		JOIN users u ON u.id = c.user_id
+		WHERE lesson_id = $1`, lessonID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var comments []comment.Comment
+	for rows.Next() {
+		var c comment.Comment
+		err := rows.Scan(&c.ID, &c.LessonID, &c.UserID, &c.UserName, &c.Body, &c.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, c)
+	}
+	return comments, nil
+}
