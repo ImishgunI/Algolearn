69921805cf3aa8028da36f85e512c2dcaa34a8ce
Michael <mvasilyev04@gmail.com>
2026-05-11 20:18:02 +0300

    Добавил методы создания удаления и обновления уроков

--- internal/infrastructure/db/lesson/lesson_repository.go
+++ internal/infrastructure/db/lesson/lesson_repository.go
@@ -4,6 +4,7 @@ import (
 	"Algolearn/internal/infrastructure/db/sql"
 	"Algolearn/internal/learning/lesson"
 	"context"
+	"time"
 )
 
 type Repository struct {
@@ -46,4 +47,33 @@ func (r *Repository) GetByID(ctx context.Context, id int) (*lesson.Lesson, error
 		return nil, err
 	}
 	return &l, nil
+}
+
+func (r *Repository) Create(ctx context.Context, lesson *lesson.Lesson) error {
+	lesson.CreatedAt = time.Now().UTC()
+	_, err := r.p.Pool.Exec(ctx, `
+	INSERT INTO lessons (course_id, title, theory, algorithm_type, order_index, created_at)
+	VALUES ($1, $2, $3, $4, $5, $6)`, lesson.CourseID, lesson.Title, lesson.Theory, lesson.AlgorithmType, lesson.OrderIndex, lesson.CreatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (r *Repository) Update(ctx context.Context, id int, lesson *lesson.Lesson) error {
+	lesson.CreatedAt = time.Now().UTC()
+	_, err := r.p.Pool.Exec(ctx,
+		`UPDATE lessons
+		SET course_id = $1, title = $2, theory = $3, algorithm_type = $4, order_index = $5, created_at = $6
+		WHERE id = $7`, lesson.CourseID, lesson.Title, lesson.Theory, lesson.AlgorithmType, lesson.OrderIndex, lesson.CreatedAt, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (r *Repository) Delete(ctx context.Context, id int) error {
+	_, err := r.p.Pool.Exec(ctx, `DELETE FROM lessons WHERE id = $1`, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
